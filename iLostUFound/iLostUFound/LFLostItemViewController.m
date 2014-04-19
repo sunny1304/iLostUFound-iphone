eb15a0f624ed92073e3a51a84f7016f0a5156445
sunny1304 <sunny1304.ad@gmail.com>
2014-04-19 10:31:51 +0600

    adding browsing controllers

---
+++ iLostUFound/iLostUFound/LFLostItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  LFLostItemViewController.m
+//  iLostUFound
+//
+//  Created by sunny on 4/19/14.
+//  Copyright (c) 2014 sunny. All rights reserved.
+//
+
+#import "LFLostItemViewController.h"
+
+@interface LFLostItemViewController ()
+
+@end
+
+@implementation LFLostItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
